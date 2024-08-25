d150b58b5e973a94cf1f7eaba24f179f2e4a2429
Aiden-Jeon <aiden.jongseob@gmail.com>
2024-08-25 16:13:09 +0900

    migration from origin github repo
    Signed-off-by: Aiden-Jeon <aiden.jongseob@gmail.com>

---
+++ docs/installation.md
@@ -0,0 +1,11 @@
+---
+sidebar_position: 1
+---
+
+# Installation
+
+이번 튜토리얼에서 사용되는 패키지를 설치합니다.
+
+```bash
+pip install torch torchvision
+```
---
+++ src/components/HIghlight.tsx
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React, { type ReactNode } from 'react';
+
+
+export function Part({
+    children,
+}: {
+    children: ReactNode;
+}): JSX.Element {
+    return (
+        <span
+            style={{
+                backgroundColor: '#9747FF',
+                borderRadius: '2px',
+                color: '#fff',
+                padding: '0.2rem',
+            }}>
+            {children}
+        </span>
+    );
+}
+
+export function Chapter({
+    children,
+}: {
+    children: ReactNode;
+}): JSX.Element {
+    return (
+        <span
+            style={{
+                backgroundColor: '#6A77D7',
+                borderRadius: '2px',
+                color: '#fff',
+                padding: '0.2rem',
+            }}>
+            {children}
+        </span>
+    );
+}
