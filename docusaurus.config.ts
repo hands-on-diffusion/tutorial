570af863bfdd2bb129006a05ef002cc28a292dce
Aiden-Jeon <aiden.jongseob@gmail.com>
2024-10-04 11:35:46 +0900

    add new codeblock for output
    Signed-off-by: Aiden-Jeon <aiden.jongseob@gmail.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,6 +1,9 @@
 import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
 
 const config: Config = {
   title: 'Hands on diffusion',
@@ -28,7 +31,15 @@ const config: Config = {
     defaultLocale: 'en',
     locales: ['en'],
   },
-
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
+  ],
   presets: [
     [
       'classic',
@@ -39,6 +50,8 @@ const config: Config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/hands-on-diffusion/tutorial/tree/main/',
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex],
         },
         // blog: {
         //   showReadingTime: true,
